98e556ca7af66051dddd1c6ae1cc86b46037fa92
ampohare <amolpohare358@gmail.com>
2018-02-06 23:41:36 +0530

    aaaaaaaaaaaaa

---
+++ Polymorphism/src/com/aml/methodoverriding/M.java
@@ -0,0 +1,15 @@
+package com.aml.methodoverriding;
+
+public class M {
+ 
+	void show() {
+		System.out.println("this is parent class show method");
+	}
+	void display() {
+		System.out.println("this is parent class display method");
+	}
+	void m() {
+		System.out.println("this is parent class m method");
+	}
+	
+}
